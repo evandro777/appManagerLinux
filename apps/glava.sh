cff56e704233950a4e536c5cb897c442ea2693ed
evandro777 <evandro777@hotmail.com>
2021-09-13 21:50:16 -0300

    added glava not official repository and some fix in vscode script

---
+++ apps/glava.sh
@@ -0,0 +1,35 @@
+#CLEMENTINE > OFFICIAL
+#COLORS
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color / Reset color
+echo -e "${ORANGE}Installing GLava - Thirdparty not official repository (linuxuprising/apps)${NC}"
+
+sudo add-apt-repository ppa:linuxuprising/apps
+sudo apt update
+sudo apt install glava
+
+glava --copy-config
+
+
+echo '
+Display monitors and resolutions
+	xrandr | grep " connected"
+	OR
+	xrandr | grep "*" | sed "s/|/ /" | awk '{print $1}'
+
+Config is located at:
+	~/.config/glava/rc.glsl
+
+	Better performance, disable it on fullscreen applications, set:
+		setfullscreencheck true
+
+force-mod options: bars, radial, graph, wave or circle
+
+Configure screen resolution and monitor to display
+	--request="setgeometry 1920 0 1360 768"
+
+Display fullscreen on first monitor
+	glava --desktop --force-mod=radial --request="setgeometry 0 0 1920 1080"
+
+Display fullscreen on second monitor
+	glava --desktop --force-mod=radial --request="setgeometry 1920 0 1360 768"'
--- apps/vscode.sh
+++ apps/vscode.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+. "$DIR/../includes/essentials.sh"
+. "../includes/root_restrict_but_sudo.sh"
+
 #COLORS
 ORANGE='\033[0;33m'
 NC='\033[0m' # No Color / Reset color
 echo -e "${ORANGE}Installing VSCode${NC}"
-
-#######################
-##### ROOT ACCESS #####
-#######################
-if [ $EUID != "0" ]; then
-	echo "Must be run as root!" 1>&2
-	#exit 1
-	if [ -t 1 ]; then
-	  exec sudo -- "$0" "$@"
-	else
-	  exec gksudo -- "$0" "$@"
-	fi
-fi
 
 #VSCODE repository official
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -37,7 +29,6 @@ sudo apt install -y code
 #Settings:
 printf '{
 	"editor.renderWhitespace": "all",
-	"editor.insertSpaces": false,
 	"editor.minimap.enabled": false,
 	"editor.fontFamily": "'"'"'Fira Code'"'"', '"'"'Droid Sans Mono'"'"', '"'"'monospace'"'"', monospace, '"'"'Droid Sans Fallback'"'"'",
 	"editor.fontLigatures": true,
@@ -47,43 +38,41 @@ printf '{
 	"[markdown]": {
 		"editor.wordWrap": "off",
 	}
-}' > ~/.config/Code/User/settings.json
-#FIX PERMISSION
-chown -R "$SUDO_USER":"$SUDO_USER" ~/.config/Code/User/settings.json
+}' | tee -a ~/.config/Code/User/settings.json > /dev/null
 
 #Plugins
 #highlight .htaccess
-sudo -u $SUDO_USER -H code --install-extension mrmlnc.vscode-apache
+#sudo -u $SUDO_USER -H code --install-extension mrmlnc.vscode-apache
 
 #highlight .env
-sudo -u $SUDO_USER -H code --install-extension mikestead.dotenv
+#sudo -u $SUDO_USER -H code --install-extension mikestead.dotenv
 
 #Highlight TODO:, FIXME:
-sudo -u $SUDO_USER -H code --install-extension wayou.vscode-todo-highlight
+#sudo -u $SUDO_USER -H code --install-extension wayou.vscode-todo-highlight
 
 #PHPDoc
-sudo -u $SUDO_USER -H code --install-extension neilbrayfield.php-docblocker
+#sudo -u $SUDO_USER -H code --install-extension neilbrayfield.php-docblocker
 
 #PHP INTELEPHENSE
-sudo -u $SUDO_USER -H code --install-extension bmewburn.vscode-intelephense-client
+#sudo -u $SUDO_USER -H code --install-extension bmewburn.vscode-intelephense-client
 
 #VUE
-sudo -u $SUDO_USER -H code --install-extension octref.vetur
+#sudo -u $SUDO_USER -H code --install-extension octref.vetur
 
 #Code Runner
-sudo -u $SUDO_USER -H code --install-extension formulahendry.code-runner
+#sudo -u $SUDO_USER -H code --install-extension formulahendry.code-runner
 
 #Class import checker (if not using an import)
-sudo -u $SUDO_USER -H code --install-extension marabesi.php-import-checker
+#sudo -u $SUDO_USER -H code --install-extension marabesi.php-import-checker
 
 #Markdown (.md) lint
-sudo -u $SUDO_USER -H code --install-extension davidanson.vscode-markdownlint
+#sudo -u $SUDO_USER -H code --install-extension davidanson.vscode-markdownlint
 
 #Markdown table formater (beautify)
-sudo -u $SUDO_USER -H code --install-extension shuworks.vscode-table-formatter
+#sudo -u $SUDO_USER -H code --install-extension shuworks.vscode-table-formatter
 
 #Local history
-sudo -u $SUDO_USER -H code --install-extension xyz.local-history
+#sudo -u $SUDO_USER -H code --install-extension xyz.local-history
 
 #Settings:
 printf '// Place your key bindings in this file to overwrite the defaults
@@ -108,6 +97,4 @@ printf '// Place your key bindings in this file to overwrite the defaults
 		"command": "-editor.action.copyLinesDownAction",
 		"when": "editorTextFocus && !editorReadonly"
 	}
-]' > ~/.config/Code/User/keybindings.json
-#FIX PERMISSION
-chown -R "$SUDO_USER":"$SUDO_USER" ~/.config/Code/User/keybindings.json
+]' | tee -a ~/.config/Code/User/keybindings.json > /dev/null
