4cc84605b717e7b9b6af7639b57be2bc25956977
evandro777 <evandro777@hotmail.com>
2021-11-29 23:35:40 -0300

    Updated insync repository & docker with updated docker composer

--- apps/docker.sh
+++ apps/docker.sh
@@ -12,3 +12,15 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io
 #execute docker without sudo
 sudo usermod -aG docker ${USER}
 su - ${USER}
+
+echo "Downloading and installing docker-composer"
+latestDockerComposeVersion="$(curl -sL https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')"
+kernelName="$(uname -s | awk '{print tolower($0)}')"
+machineHardwareName="$(uname -m)"
+dockerComposeUrlDownload="https://github.com/docker/compose/releases/download/$latestDockerComposeVersion/docker-compose-$kernelName-$machineHardwareName"
+
+echo "Latest docker-composer version found: $latestDockerComposeVersion"
+
+sudo curl -L "$dockerComposeUrlDownload" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
--- apps/insync-rep.sh
+++ apps/insync-rep.sh
@@ -1,7 +1,7 @@
 #Insync > Official Repository:
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
-#echo deb http://apt.insynchq.com/ubuntu xenial non-free contrib | sudo tee /etc/apt/sources.list.d/insync.list
-echo deb http://apt.insynchq.com/mint serena non-free contrib | sudo tee /etc/apt/sources.list.d/insync.list
+#echo deb http://apt.insync.io/ubuntu xenial non-free contrib | sudo tee /etc/apt/sources.list.d/insync.list
+echo deb http://apt.insync.io/mint uma non-free contrib | sudo tee /etc/apt/sources.list.d/insync.list
 sudo apt update && sudo apt install -y insync
 
 read -p "Press [Enter] to continue."
