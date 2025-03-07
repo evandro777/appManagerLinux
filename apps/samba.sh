54750bce667516cfe41d81a0655a793c39993a71
evandro777 <seven.skull@gmail.com>
2025-03-07 16:34:47 -0300

    Samba: fixed firewall wide links is a parameter option

--- apps/samba.sh
+++ apps/samba.sh
@@ -27,12 +27,17 @@ function perform_install() {
     declare -A params=$1
     create_samba_user $params
 
-    set_samba_conf
+    set_samba_conf $params
 
     if command -v ufw &> /dev/null; then
         echo "UFW firewall detected. Creating exception rules"
-        sudo ufw allow from 192.168.0.0/24 to any app Samba comment "Samba: Only local network"
+
+        sudo ufw allow from 192.168.0.0/16 to any app Samba comment "Samba: Only local network"
+        sudo ufw allow from 10.0.0.0/8 to any app Samba comment "Samba: Only local network"
+        sudo ufw allow from 172.16.0.0/12 to any app Samba comment "Samba: Only local network"
+
         sudo ufw deny Samba comment "Samba: Only local network"
+
         echo "Showing UFW created rules"
         sudo ufw status | grep "Samba: Only local network"
     fi
@@ -42,11 +47,15 @@ function perform_install() {
     echo -e "${GREEN}    * Use nemo and go to a folder (like home: $HOME)${NC}"
     echo -e "${GREEN}    * Right click on the folder (like: Public) to share > Sharing options${NC}"
     echo -e "${GREEN}    * Share this folder > Create share${NC}"
+    echo -e "${GREEN}Folders automatically shared:${NC}"
+    echo -e "${GREEN}    * Everyone on network can access, read & write: ${SHARE_FOLDER_PUBLIC}${NC}"
+    echo -e "${GREEN}    * Logged user (${USER}) on network can access and read: ${SHARE_FOLDER_READ}${NC}"
+    echo -e "${GREEN}    * Logged user (${USER}) on network can access, read & write: ${SHARE_FOLDER_WRITE}${NC}"
     echo -e ""
     echo -e "${YELLOW}The network login will be:${NC}"
     echo -e "${YELLOW}Username:${NC} ${USER}"
     echo -e "${YELLOW}Domain:${NC} WORKGROUP"
-    echo -e "${YELLOW}Password:${NC} 'The password used for samba'"
+    echo -e "${YELLOW}Password:${NC} 'The password set for Samba'"
 
 }
 
@@ -86,15 +95,16 @@ function perform_check() {
 function get_parameters() {
     # Don't use echo before read, it's not going to work to set into a variable "app-menu-init.sh"
     while true; do
-        read -rsp $'Type a password for ⚠️ Samba user ⚠️ ('$USER'): ' password
-        echo # Add a linebreak
+        # quote using read is a problem using a variable and doesn't work with \n, so this is a workaround: '"$USER"$'
+        read -rsp $'Type a password for ⚠️ Samba user ⚠️ ('"$USER"$'). Cannot be empty: \n' password
         if [[ -n "$password" ]]; then
             break # Exit loop if password is not empty
         fi
-        echo "⚠️  Password cannot be empty! Please try again."
     done
 
-    echo "--samba-password=$password"
+    read -rp $'Enable wide links [y | n (default)]\nCaution: samba can follow symlinks anywhere, which can be dangerous. Default is no (safer): ' enable_wide_links
+
+    echo "--samba-password=$password --samba-enable-wide-links=$enable_wide_links"
 }
 
 function create_folders_and_sharing() {
@@ -173,6 +183,9 @@ function create_samba_user() {
 }
 
 function set_samba_conf() {
+    # Access the associative array passed as a parameter
+    local -n params=$1
+
     echo "Creating smb.conf backup at (doesn't overwrite): $SAMBA_CONF_BK"
     sudo cp --update=none "$SAMBA_CONF" "$SAMBA_CONF_BK"
 
@@ -186,20 +199,22 @@ function set_samba_conf() {
     #Symlinks
     echo "Enable follow symlinks"
     sudo crudini --set "$SAMBA_CONF" "global" "follow symlinks" "yes"
-    echo "Enable wide links. Caution: samba can follow links anywhere, which can be dangerous"
-    sudo crudini --set "$SAMBA_CONF" "global" "wide links" "yes"
+    if [ -n "${params[samba\-enable\-wide\-links]}" ]; then
+        echo "Enable wide links. Caution: samba can follow links anywhere, which can be dangerous"
+        sudo crudini --set "$SAMBA_CONF" "global" "wide links" "yes"
+    fi
 
     #Performance
-    echo "Enable sendfile: SMB2 > Improve performance. Might have problems sharing NFS, CIFS or old SMB versions"
+    echo "Enable sendfile > Improve performance. Might have problems sharing NFS, CIFS or old SMB versions"
     sudo crudini --set "$SAMBA_CONF" "global" "use sendfile" "yes"
     echo "Disable strict locking > Improve performance. Avoid unnecessary file blocking"
     sudo crudini --set "$SAMBA_CONF" "global" "strict locking" "no"
 
     #Security
-    echo "Set client min protocol: SMB2 > Useful for security and performance reasons"
-    sudo crudini --set "$SAMBA_CONF" "global" "client min protocol" "SMB2" #avoid SMB1 for security and performance reasons
-    echo "Set server min protocol: SMB2 > Useful for security and performance reasons"
-    sudo crudini --set "$SAMBA_CONF" "global" "server min protocol" "SMB2" #avoid SMB1 for security and performance reasons
+    echo "Set client min protocol: SMB3 > Useful for security and performance reasons. Windows 8+"
+    sudo crudini --set "$SAMBA_CONF" "global" "client min protocol" "SMB3" #avoid SMB1 for security and performance reasons
+    echo "Set server min protocol: SMB3 > Useful for security and performance reasons. Windows 8+"
+    sudo crudini --set "$SAMBA_CONF" "global" "server min protocol" "SMB3" #avoid SMB1 for security and performance reasons
     echo "Disable ntlm auth > Useful for security reasons"
     sudo crudini --set "$SAMBA_CONF" "global" "ntlm auth" "no"
 
