2f29154953baf0384b0cdc6810041042da98300a
evandro777 <seven.skull@gmail.com>
2025-07-24 20:36:39 -0300

    Meld: custom config

--- apps/meld.sh
+++ apps/meld.sh
@@ -6,10 +6,19 @@ readonly APPLICATION_ID="meld"
 
 function perform_install() {
     package_install "$APPLICATION_ID"
+
+    dconf write /org/gnome/meld/indent-width 4
+    dconf write /org/gnome/meld/insert-spaces-instead-of-tabs true
+    dconf write /org/gnome/meld/highlight-current-line true
+    dconf write /org/gnome/meld/show-line-numbers true
+    dconf write /org/gnome/meld/prefer-dark-theme true
+    dconf write /org/gnome/meld/highlight-syntax true
+    dconf write /org/gnome/meld/style-scheme "'dracula'"
 }
 
 function perform_uninstall() {
     package_uninstall "$APPLICATION_ID"
+    dconf reset -f /org/gnome/meld/
 }
 
 function perform_check() {
